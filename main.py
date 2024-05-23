c80595d536aba6f39617bdfd70b3d59812ba7f18
hjuraev31 <hjuraev31@gmail.com>
2024-05-24 01:04:55 +0500

    feat: split functions

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from flask import Flask, jsonify, request
 from folder_manager import generate_unique_folder
 import os
 from msg_parser import parse_message
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def main():
     return {'message' : 'Test run successfully!'}
 
-@app.route('/upload_msg', methods=['POST'])
+@app.route('/api/upload_msg', methods=['POST'])
 def upload_msg():
 
     uploaded_file = request.files['file']
@@ -21,18 +23,38 @@ def upload_msg():
         destination = os.path.join(unique_folder_path,uploaded_file.filename)
         uploaded_file.save(destination)
 
-        base_directory = r"C:\Users\hjura\OneDrive\Рабочий стол\SWE\msg-viewer"
+        base_directory = r"C:\Users\hjura\OneDrive\Рабочий стол\SWE\msg-viewer-backend\msg-viewer"
         full_path = os.path.join(base_directory, destination)
         parsed_msg = parse_message(full_path, os.path.join(base_directory,unique_folder_path))
 
         return {
-            'data':'File Uploaded Successfully',
+            'message':'success',
             'uuid':unique_folder_path, 
             'result':parsed_msg,
         }
 
     else:
         return {'error':'File upload failed, or invalid file type'}
+
+@app.route('/delete_report/', methods=['POST'])
+def delete_file():
+    return 1
+
+
+@app.route('/api/msg_parser/<string:uuid>', methods=['POST'])
+def msg_email_parser(uuid):
+
+    destination = os.path.join('uploads/'+ uuid,'message.msg')
+    base_directory = r"C:\Users\hjura\OneDrive\Рабочий стол\SWE\msg-viewer-backend\msg-viewer"
+    full_path = os.path.join(base_directory, destination)
+    parsed_msg = parse_message(full_path, os.path.join(base_directory,'uploads/'+uuid))
+
+    result = {
+        "message": "Key received successfully",
+        "data": parsed_msg,
+    }
+    
+    return jsonify(result), 200
 
 
 def allowed_file(filename):
